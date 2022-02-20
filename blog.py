689f97f5dff58b6d07864e932083ec10fff25953
gulsahsahin <guulssahh@gmail.com>
2022-02-20 23:47:13 +0300

    login

--- blog.py
+++ blog.py
@@ -2,15 +2,20 @@ from flask import Flask, render_template, flash, redirect, url_for, session, log
 import firebase_admin
 from firebase_admin import credentials, firestore
 from datetime import datetime
+from flask_session import Session
 
 cred = credentials.Certificate(
     "ybblog-506ca-firebase-adminsdk-rr5mm-4c893a30b7.json")
-firebase_admin.initialize_app(cred)
+pb = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 app = Flask(__name__)
-app.secret_key = "ybblog"  # flash mesajları gözükmesi için
-#app.config['SECRET_KEY'] = 'kmakmakma'
+app.secret_key = "ybblog"# flash mesajları gözükmesi için
+# app.config['SECRET_KEY'] = 'kmakmakma'
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 
 """
 @app.route("/")
@@ -29,6 +34,9 @@ def index():
 
 @app.route("/")
 def index():
+    users = db.collection("users")
+    all_users = [doc.to_dict() for doc in users.stream()]
+    print(len(all_users))
     return render_template("index.html")
 
     # articles = [
@@ -42,7 +50,8 @@ def index():
 def about():
     return render_template("about.html")
 
-@app.route("/dashboard")
+
+@app.route("/dashboard", methods=["GET", "POST"])
 def dashboard():
     return render_template("dashboard.html")
 
@@ -50,7 +59,6 @@ def dashboard():
 @app.route("/article/<string:id>")  # Dinamik URL
 def detail(id):
     return "Article Id: "+id
-
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -62,62 +70,65 @@ def register():
         password = request.form["password"]
         kayityolu = db.collection("users").document()
         kayityolu.set({
-            "name":name,
-            "username":username,
-            "email":email,
-            "password":password,
-            "key":kayityolu.id,
-            "date":datetime.now()
+            "name": name,
+            "username": username,
+            "email": email,
+            "password": password,
+            "key": kayityolu.id,
+            "date": datetime.now()
         })
-        flash("Başarıyla kayıt oldunuz...", "success")    
-        return redirect(url_for("login")) #gitmek istediğin sayfanın fonksiyon adını ver
-    kayityolu = db.collection("users")
-    gelenveri = [doc.to_dict() for doc in kayityolu.stream()]
+        flash("Başarıyla kayıt oldunuz...", "success")
+        # gitmek istediğin sayfanın fonksiyon adını ver
+        return redirect(url_for("login"))
+    kayit = db.collection("users")
+    gelenveri = [doc.to_dict() for doc in kayit.stream()]
     return render_template("register.html", gelenveri=gelenveri)
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    error = None
     if request.method == "POST":
+        #Forma girilen veriler alındı.
         username = request.form["username"]
         password = request.form["password"]
-        kayityolu = db.collection("login").document()
-        kayityolu.set({
-            "username":username,
-            "password":password,
-            "key":kayityolu.id,
-            "date":datetime.now()
-        })
-        result=len(kayityolu["username"])
-        if result>0:
-            data = kayityolu.fetchone()
-            if data["password"]==kayityolu["password"]:
-                flash("Giriş Yapıldı", "success")
+        
+        #veri tabanından bütün kullanıcılar çekildi
+        users = db.collection("users")
+        all_users = [doc.to_dict() for doc in users.stream()]
+        
+        #veri tabanından gelen kullanıcılar kadar for döngüsü döndürülüp her kullanıcı x değerine atandı
+        for x in all_users:
+            #veritabanından gelen kullanıcılar arasında forma girilen kullanıcı adına sahip biri varmı kontrol edildi
+            if username == x['username']:
+                #kullanıcı adı mevcut ise parolası dbpassword değişkenine atandı
+                dbpassword = x['password']
                 
-                session["logged_in"] = True
-                session["username"] = username
-                                
-                return redirect(url_for("index"))
+                #formdan gelen password ile kullanıcının veri tabanından gelen passwordu karşılaştırıldı
+                if password == dbpassword:
+                    #giriş başarılı ise session a username atandı
+                    session["username"] = username
+                    flash("Başarıyla giriş yaptınız...", "success")
+                    return redirect(url_for("index"))
+                else:
+                    flash("Şifreniz yanlış...", "danger")
+                    return redirect(url_for("login"))
             else:
-                flash("Parola hatalı", "danger")
+                flash("Böyle bir kullanıcı bulunamadı...", "danger")
                 return redirect(url_for("login"))
-            
-        else:
-            flash("Böyle bir kullanıcı bulunmuyor!", "danger")
-            return redirect(url_for("login"))
-    kayityolu = db.collection("login")
-    gelenveri = [doc.to_dict() for doc in kayityolu.stream()]
-    return render_template("login.html", gelenveri=gelenveri)
+
+    return render_template('login.html', error=error)
+
+
+@app.route("/_formhelpers", methods=["GET", "POST"])
+def formhelpers():
+    return render_template("_formhelpers.html")
+
 
 @app.route("/logout")
 def logout():
     session.clear()
     return redirect(url_for("index"))
-
-
-
-
-
 
 
 if __name__ == "__main__":
