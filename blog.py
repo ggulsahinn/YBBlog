ef5c2791be92cd2c547a9b8d14e44673fee48217
gulsahsahin <guulssahh@gmail.com>
2022-02-22 12:32:12 +0300

    makale_list

--- blog.py
+++ blog.py
@@ -1,4 +1,3 @@
-import re
 from flask import Flask, render_template, flash, redirect, url_for, session, logging, request, Response, jsonify
 import firebase_admin
 from firebase_admin import credentials, firestore
@@ -13,7 +12,7 @@ pb = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 app = Flask(__name__)
-app.secret_key = "ybblog" # flash mesajları gözükmesi için
+app.secret_key = "ybblog"  # flash mesajları gözükmesi için
 # app.config['SECRET_KEY'] = 'kmakmakma'
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
@@ -21,7 +20,8 @@ Session(app)
 app.config['CKEDITOR_PKG_TYPE'] = 'basic'
 ckeditor = CKEditor(app)
 
-#Kullanıcı Giriş Decorator'ı
+# Kullanıcı Giriş Decorator'ı
+
 
 def login_required(f):
     @wraps(f)
@@ -33,12 +33,11 @@ def login_required(f):
             return redirect(url_for("login"))
     return decorated_function
 
+
 @app.route("/")
 def index():
-    users = db.collection("users")
-    all_users = [doc.to_dict() for doc in users.stream()]
-    print(len(all_users))
     return render_template("index.html")
+
 
 @app.route("/about")
 def about():
@@ -51,9 +50,20 @@ def dashboard():
     return render_template("dashboard.html")
 
 
+@app.route("/articles", methods=["GET", "POST"])
+def articles():
+    article = db.collection("articles")
+    all_article = [doc.to_dict() for doc in article.stream()]
+    if len(all_article) > 0:
+        return render_template("articles.html", all_article=all_article)
+    else:
+        return render_template("articles.html")
+
+
 @app.route("/article/<string:id>")  # Dinamik URL
 def detail(id):
     return "Article Id: "+id
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -83,24 +93,24 @@ def register():
 def login():
     error = None
     if request.method == "POST":
-        #form a girilen veriler alındı.
+        # form a girilen veriler alındı.
         username = request.form["username"]
         password = request.form["password"]
-        
-        #veri tabanından bütün kullanıcılar çekildi
+
+        # veri tabanından bütün kullanıcılar çekildi
         users = db.collection("users")
         all_users = [doc.to_dict() for doc in users.stream()]
-        
-        #veri tabanından gelen kullanıcılar kadar for döngüsü döndürülüp her kullanıcı x değerine atandı
+
+        # veri tabanından gelen kullanıcılar kadar for döngüsü döndürülüp her kullanıcı x değerine atandı
         for x in all_users:
-            #veritabanından gelen kullanıcılar arasında forma girilen kullanıcı adına sahip biri varmı kontrol edildi
+            # veritabanından gelen kullanıcılar arasında forma girilen kullanıcı adına sahip biri varmı kontrol edildi
             if username == x['username']:
-                #kullanıcı adı mevcut ise parolası dbpassword değişkenine atandı
+                # kullanıcı adı mevcut ise parolası dbpassword değişkenine atandı
                 dbpassword = x['password']
-                
-                #formdan gelen password ile kullanıcının veri tabanından gelen passwordu karşılaştırıldı
+
+                # formdan gelen password ile kullanıcının veri tabanından gelen passwordu karşılaştırıldı
                 if password == dbpassword:
-                    #giriş başarılı ise session a username atandı
+                    # giriş başarılı ise session a username atandı
                     session["logged_in"] = True
                     session["username"] = username
                     flash("Başarıyla giriş yaptınız...", "success")
@@ -114,6 +124,7 @@ def login():
 
     return render_template('login.html', error=error)
 
+
 @app.route("/addarticle", methods=["GET", "POST"])
 def addarticle():
     if request.method == "POST":
@@ -122,26 +133,28 @@ def addarticle():
         content = request.form["content"]
         kayityolu = db.collection("articles").document()
         kayityolu.set({
-            "id": id,
             "title": title,
             "author": author,
             "content": content,
             "key": kayityolu.id,
             "date": datetime.now()
         })
-        return redirect(url_for(addarticle))
+        return redirect(url_for("addarticle"))
     kayit = db.collection("articles")
     gelenveri = [doc.to_dict() for doc in kayit.stream()]
     return render_template("addarticle.html", gelenveri=gelenveri)
+
 
 @app.route("/logout")
 def logout():
     session.clear()
     return redirect(url_for("index"))
 
+
 @app.route("/_formhelpers", methods=["GET", "POST"])
 def formhelpers():
     return render_template("_formhelpers.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
